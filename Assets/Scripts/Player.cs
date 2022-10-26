08af1594bf2ee7a6ad458d049d9d0210b01c329f
24-LowRongJun <damienlowrj2003@gmail.com>
2022-10-26 10:06:52 +0800

    restrict player movement + gameover scene

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+        if(transform.position.y <= -3 || transform.position.y >= 5  )
+        {
+            transform.position = new Vector3(-8.19f, -0.128f, 0.008284654f);
+        }
+
+      
+
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "border")
+        {
+
+        }
+    }
+}
